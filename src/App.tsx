a2c3d7edd674271bb69a35a4638bba12c46ecdf2
lufarius <infinitevolume@gmail.com>
2022-12-11 11:44:58 -0800

    added basic search functionality to App

--- src/App.tsx
+++ src/App.tsx
@@ -40,32 +40,36 @@ function App() {
         setSearchTerm(event.target.value);
     }
 
-    async function getUser() {
-        const response = await axios.get(query).then(({ data }) => data);
-        setGitHubUser(response);
-    }
-
     function handleSubmit() {
         setUserName(searchTerm);
-        getUser();
     }
 
-    console.log(gitHubUser);
-    useEffect(() => {
-        if (userName === "") return;
-        let mounted = true;
-        let prevUserName = userName;
+    function formatDate() {
+        const date = new Date(created_at);
+        const options = {
+            day: "2-digit",
+            month: "short",
+            year: "numeric",
+        };
+        const formattedDate = date.toLocaleDateString("en-US", options).split(" ");
+        [formattedDate[0], formattedDate[1]] = [formattedDate[1], formattedDate[0]];
+        formattedDate[0] = formattedDate[0].slice(0, 2);
+        const result = formattedDate.join(" ");
 
+        return result;
+    }
+
+    useEffect(() => {
         async function getUserData() {
             const response = await axios.get(query).then(({ data }) => {
+                console.log(data);
                 return data;
             });
-
             setGitHubUser(response);
         }
-
         getUserData();
-    }, []);
+    }, [userName]);
+
     return (
         <div className="App">
             <main>
@@ -73,9 +77,32 @@ function App() {
                 <label htmlFor="search">username</label>
                 <input id="search" type="text" onChange={handleChange} value={searchTerm} />
                 <button onClick={handleSubmit}>Search</button>
+                <div>
+                    <p>{name}</p>
+                    <p>{login}</p>
+                    <img src={avatar_url} alt={`$Github user${name}`} />
+                    <span>
+                        Joined <span>{formatDate()}</span>
+                    </span>
+                </div>
             </main>
         </div>
     );
+
+    // return (
+    //     <div>
+    //         <input type="text" value={searchTerm} onChange={handleChange} />
+    //         <button onClick={handleSubmit}>Search</button>
+    //         {gitHubUser ? (
+    //             <div>
+    //                 <h1>{gitHubUser.name}</h1>
+    //                 <p>{gitHubUser.bio}</p>
+    //             </div>
+    //         ) : (
+    //             <p>Enter a GitHub username to fetch data.</p>
+    //         )}
+    //     </div>
+    // );
 }
 
 export default App;
